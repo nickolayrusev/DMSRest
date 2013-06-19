4951b19fe7cdaca390cc2a09cd4c64455ca3390e
nickolayrusev <nickolay.rusev@gmail.com>
2013-06-19 18:46:26 +0300

    now the app returns 14 results per page

--- DMSBgRest/src/main/java/org/mongo/controller/HomeController.java
+++ DMSBgRest/src/main/java/org/mongo/controller/HomeController.java
@@ -3,8 +3,6 @@ package org.mongo.controller;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.mongo.domain.Campaign;
 import org.mongo.utils.CommonUtils;
 import org.slf4j.Logger;
@@ -14,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 /**
  * Handles requests for the application home page.
@@ -27,12 +28,19 @@ public class HomeController {
 	@ResponseBody
 	public List<Campaign> campaigns(
 			@RequestParam(required = false, value = "page") Integer page,
-			@RequestParam(required = false, value = "type") Integer type, HttpServletResponse response)
+			@RequestParam(required = false, value = "type") Integer type)
 			throws IOException {
 		logger.info("page is: " + page + "type is: " + type);
-		page = page==null || page<=1 ? 1 : page;
-		List<Campaign> lstCampaigns = CommonUtils.parseCampaignByPage(page-1,type);
-		return lstCampaigns;
+		//page = page==null || page<=1 ? 1 : page;
+		int firstPage = (page-1) * 2;
+		int secondPage = page+(page-1);
+		logger.info("firstpage:"+firstPage + " secondPage:"+secondPage);
+		List<Campaign> lstCampaignsFirstPage = CommonUtils.parseCampaignByPage(firstPage,type);
+		List<Campaign> lstCampaignsSecondPage = CommonUtils.parseCampaignByPage(secondPage,type);
+		List<Campaign> mergedList = Lists.newArrayList( Iterables.concat(lstCampaignsFirstPage,lstCampaignsSecondPage) );
+		
+		logger.info("first list size: "+lstCampaignsFirstPage.size() +" second list size: "+lstCampaignsSecondPage.size()+" merged list size: "+mergedList.size());
+		return mergedList;
 	}
 	
 }
